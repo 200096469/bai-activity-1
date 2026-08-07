56bd64081beef10e622d90aecdc84eb18008d77c
Roberto Palozzo <roberto.palozzo@gmail.com>
2026-08-07 19:35:55 +1000

    Second Commit to update Activity 1

--- activity.js
+++ activity.js
@@ -1,4 +1,6 @@
 function changeColour() {
+    var colour = document.getElementById("textColourValue").value;
+    document.getElementById("pageBody").style.color = colour;
     var colour = document.getElementById("colour").value;
     document.getElementById("pageBody").style.backgroundColor = colour;
     if (colour === '') {
@@ -16,6 +18,8 @@ function changeColour() {
 function resetColour() {
     var colour = ''; // Reset to default colour
     document.getElementById("pageBody").style.backgroundColor = 'white'; // Set background to default
+    document.getElementById("pageBody").style.color = 'black'; // Set text colour to default
     document.getElementById("chosenColour").textContent = colour;
-    document.getElementById("colour").value = ''; // Clear the input field
+    document.getElementById("colour").value = ''; // Clear the choose colourinput field
+    document.getElementById("textColourValue").value = ''; // Clear the text colour input field
 }
