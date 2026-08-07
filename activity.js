f3bd90261150585ef7d94a117bdbd7acea9c5126
Roberto Palozzo <roberto.palozzo@gmail.com>
2026-08-07 18:53:35 +1000

    First Commit of Activity 1 completed

---
+++ activity.js
@@ -0,0 +1,21 @@
+function changeColour() {
+    var colour = document.getElementById("colour").value;
+    document.getElementById("pageBody").style.backgroundColor = colour;
+    if (colour === '') {
+        document.getElementById("chosenColour").textContent = 'No colour chosen';
+    }
+    else {
+        document.getElementById("chosenColour").textContent = colour;
+    }
+    var lowerColour = colour.toLowerCase();
+    if (lowerColour === 'red' || lowerColour === '#ff0000') {
+        console.log("The user chose the colour red");
+    }
+}
+
+function resetColour() {
+    var colour = ''; // Reset to default colour
+    document.getElementById("pageBody").style.backgroundColor = 'white'; // Set background to default
+    document.getElementById("chosenColour").textContent = colour;
+    document.getElementById("colour").value = ''; // Clear the input field
+}
---
+++ script.js
@@ -0,0 +1,2 @@
+console.log("Hello world");
+console.log("JavaScript is linked and working!");
